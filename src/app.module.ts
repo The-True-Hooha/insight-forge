71214eeea13fcd1136133b8716a55042a6564986
Davidhero <owogogahhero@outlook.com>
2024-02-17 15:44:46 +0100

    build changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,15 +8,39 @@ import { ContentGenerationModule } from './content-generation/content-generation
 import { AnalyticsModule } from './analytics/analytics.module';
 import { LlmModule } from './llm/llm.module';
 import { CoreModule } from './core/core.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ContextModule } from './libs/context/context.module';
 import { CustomLoggerModule } from './libs/logger/logger.module';
+import { DrizzleModule } from './drizzle/drizzle.module';
+import { DrizzleTursoModule } from '@knaadh/nestjs-drizzle-turso';
+import * as schema from './drizzle/schema/schema'
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE || 'dev'}`],
       isGlobal: true,
       cache: true,
+    }),
+    DrizzleTursoModule.registerAsync({
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      tag: 'insight_forge_stage',
+      useFactory: async (configService: ConfigService) => {
+        console.log(configService.get('DB_TOKEN'));
+        return {
+          turso: {
+            config: {
+              url: configService.get('URL'),
+              authToken: configService.get('DB_TOKEN'),
+            },
+          },
+          config: {
+            schema: {
+              ...schema,
+            },
+          },
+        };
+      },
     }),
     TeamsModule,
     SecurityModule,
@@ -29,6 +53,7 @@ import { CustomLoggerModule } from './libs/logger/logger.module';
     CoreModule,
     ContextModule,
     CustomLoggerModule,
+    DrizzleModule,
   ],
   controllers: [],
   providers: [],
